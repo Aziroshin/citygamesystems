bc7981ab28d2c2ae620238952b6bb8cf265c0c09
Aziroshin <aziroshin@gmx.net>
2022-07-04 11:19:50 +0200

    ✨ F4 now saves & closes the `transform` and `motion_mode` of `PlayerWorldInterface`. Also, its inspector panel now has the options `enable_integration_warnings` and `show_integration_errors`. 💡Also did some restructuring and commenting. 🐛 The scene now actually has values configured for `config_file_path` and `config_section`, and a higher `default_speed` than the script default.

--- world_objects/PlayerWorldInterface/PlayerWorldInterface.gd
+++ world_objects/PlayerWorldInterface/PlayerWorldInterface.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
+# Note: When copy-pasting this, don't forget to also get `NilableTransform3D`
+# from `res://global_lib/nilable_types/nilable_transform3d.gd`
+
 ###########################################################################
 # Config
 ###########################################################################
-# Action names for movement
+### Action names for movement.
 const MOVE_LEFT_ACTION := "camera_left"
 const MOVE_RIGHT_ACTION := "camera_right"
 const MOVE_FORWARD_ACTION := "camera_forward"
@@ -11,15 +14,29 @@ const MOVE_BACKWARD_ACTION := "camera_backward"
 const MOVE_UP_ACTION := "camera_up"
 const MOVE_DOWN_ACTION := "camera_down"
 
+### Action names for editor-only functionality.
+# This is a special action for quick in-editor testing, which should be
+# distinct from normal "save and quit" type of actions in more complex
+# applications, and is intended to be used by this camera, not other things.
+const SAVE_AND_QUIT_EDITOR_ACTION := "dev_camera_save_and_quit"
+
+### Exports
 @export var default_speed := 128.0
 @export var overwrite_existing_actions := false
 @export var double_tap_interval := 0.2
 @export var initial_motion_mode: MotionMode = MOTION_MODE_FLOATING
-###########################################################################
+@export var config_file_path := ""
+@export var config_section := ""
+@export var enable_integration_warnings := true
+@export var show_integration_errors := true
 
+### Child nodes should get a handy variable here, which should then be used
+### in the code instead of the path, to make node tree refactoring more
+### straight forward.
 @onready var camera: Camera3D = $SpringArm/Camera
 
-# If more than a key is needed, refactor the value to be an object instead.
+### Default keys for actions.
+### If more than a key is needed, refactor the value to be an object instead.
 const action_default_keys := {
 	MOVE_LEFT_ACTION: KEY_A,
 	MOVE_RIGHT_ACTION: KEY_D,
@@ -27,26 +44,26 @@ const action_default_keys := {
 	MOVE_BACKWARD_ACTION: KEY_S,
 	MOVE_UP_ACTION: KEY_SPACE,
 	MOVE_DOWN_ACTION: KEY_SHIFT,
+	SAVE_AND_QUIT_EDITOR_ACTION: KEY_F4
 }
 
+### Config file anatomy.
+const CONFIG_VALUE_NAME_TRANSFORM := "transform"
+const CONFIG_VALUE_NAME_MOTION_MODE := "motion_mode"
+###########################################################################
+
+
+###########################################################################
+# Non-config globals
+###########################################################################
 var delta_without_up_action := 0.0
+###########################################################################
 
-func set_action_key(action: String, keycode: int):
-	var key_event_for_action := InputEventKey.new()
-	key_event_for_action.keycode = keycode
-	InputMap.action_add_event(action, key_event_for_action)
 
-func ensure_action_configured(action: String) -> void:
-	if not InputMap.has_action(action):
-		push_error("Action not found: %s." % action)
-		InputMap.add_action(action)
-	if overwrite_existing_actions:
-		set_action_key(action, action_default_keys[action])
-		
-func ensure_actions_configured()-> void:
-	for action in action_default_keys.keys():
-		ensure_action_configured(action)
-		
+###########################################################################
+# Misc
+###########################################################################
+# Get the resulting vector from adding up all vectors in the specified array.
 func force_from_forces(forces: Array[Vector3]) -> Vector3:
 	assert(len(forces) > 0, "`forces` can't be empty.")
 	
@@ -56,18 +73,131 @@ func force_from_forces(forces: Array[Vector3]) -> Vector3:
 	var force: Vector3
 	var i = 0
 	while i < len(forces) - 1:
-		print("i: %s, len: %s" % [i, len(forces)])
 		force = forces[i] + forces[i + 1]
 		i = i + 1
 	return force
-	
-func _ready():
-	ensure_actions_configured()
-	motion_mode = initial_motion_mode
+###########################################################################
 
+
+###########################################################################
+# Actions
+###########################################################################
+# Set a key for an action.
+# This alters `InputMap` at runtime and doesn't write to the project.
+func set_action_key(action: String, keycode: int):
+	var key_event_for_action := InputEventKey.new()
+	key_event_for_action.keycode = keycode
+	InputMap.action_add_event(action, key_event_for_action)
+	
+	
+# Add (and overwrite if specified) an action as required to make sure it's
+# reasonably set up as configured without having to set it up in the Input Map
+# first.
+# This alters `InputMap` at runtime and doesn't write to the project.
+func ensure_action_configured(action: String, overwrite: bool = false) -> void:
+	if not InputMap.has_action(action):
+		InputMap.add_action(action)
+		set_action_key(action, action_default_keys[action])
+		if enable_integration_warnings:
+			push_warning("Action not found: %s. Spoofing it with hotkey: %s."\
+				% [action, InputMap.action_get_events(action)[0].as_text()])
+	elif overwrite:
+		set_action_key(action, action_default_keys[action])
+		
+		
+# Add (and overwrite if specified) actions as required to provide reasonable
+# controls as configured without having to set them up in the Input Map first.
+# This alters `InputMap` at runtime and doesn't write to the project.
+func ensure_actions_configured(overwrite: bool = false) -> void:
+	for action in action_default_keys.keys():
+		ensure_action_configured(action, overwrite)
+		
+		
+# Checks whether we have everything to access the config.
+func check_config_access() -> bool:
+	if config_file_path == "" or config_section == "":
+		if show_integration_errors:
+			push_error("No config file path and/or section configured for %s" % name)
+		return false
+	return true
+###########################################################################
+
+
+###########################################################################
+# Config
+###########################################################################
+# Saves config value and returns true or fails and returns false.
+func save_config_value(section: String, key: String, value) -> bool:
+	if check_config_access():
+		var config := ConfigFile.new()
+		config.load(config_file_path)
+		config.set_value(section, key, value)
+		config.save(config_file_path)
+		return true
+	return false
+	
+	
+func load_config_value(section: String, key: String):
+	var config := ConfigFile.new()
+	config.load(config_file_path)
+	return config.get_value(config_section, key)
+	
+	
+func save_transform() -> bool:
+	return save_config_value(config_section, CONFIG_VALUE_NAME_TRANSFORM, transform)
+	
+	
+func load_transform() -> NilableTransform3D:
+	var value = load_config_value(config_section, CONFIG_VALUE_NAME_TRANSFORM)
+	if value is Transform3D:
+		return NilableTransform3D.new().set_value(value)
+	else:
+		return NilableTransform3D.new()
+		
+		
+func save_motion_mode() -> bool:
+	return save_config_value(config_section, CONFIG_VALUE_NAME_MOTION_MODE, motion_mode)
+		
+		
+func load_motion_mode() -> NilableInt:
+	var value = load_config_value(config_section, CONFIG_VALUE_NAME_MOTION_MODE)
+	if typeof(value) == TYPE_INT:
+		return NilableInt.new().set_value(value)
+	return NilableInt.new()
+###########################################################################
+
+
+###########################################################################
+# The Stuff
+###########################################################################
+func _ready():
+	# Load transform
+	var transform_data_from_config := load_transform()
+	if not transform_data_from_config.is_nil:
+		transform = transform_data_from_config.value
+		
+	# Load motion mode
+	var motion_mode_data_from_config := load_motion_mode()
+	if not motion_mode_data_from_config.is_nil:
+		motion_mode = motion_mode_data_from_config.value
+	else:
+		motion_mode = initial_motion_mode
+	
+	ensure_actions_configured(overwrite_existing_actions)
+	
+	
 func _physics_process(delta: float) -> void:
 	var forces: Array[Vector3]
 	
+	#######################################################################
+	# Input
+	#######################################################################
+	if Input.is_action_just_pressed(SAVE_AND_QUIT_EDITOR_ACTION):
+		if OS.has_feature("editor"):
+			save_transform()
+			save_motion_mode()
+			if not Engine.is_editor_hint():
+				get_tree().quit()
 	if Input.is_action_pressed(MOVE_LEFT_ACTION):
 		forces.append(transform.basis.x)
 	if Input.is_action_pressed(MOVE_RIGHT_ACTION):
@@ -93,20 +223,20 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_pressed(MOVE_DOWN_ACTION):
 		forces.append(-transform.basis.y)
+	#######################################################################
 	
-	# If we're flying, add a gravity force.
+	# If we're flying without being in floating mode, add a gravity force.
 	if not is_on_floor() and not motion_mode == MOTION_MODE_FLOATING:
 		forces.append(Vector3(0, -1, 0))
 	
+	# Combine all forces and move.
 	if len(forces) > 0:
 		velocity = force_from_forces(forces).normalized() * default_speed * delta
 		move_and_slide()
 		
-	# Wrapping up.
 	delta_without_up_action = delta_without_up_action + delta
-
+	
+	
 func _input(event: InputEvent) -> void:
 	pass
-	
-# Notes (TODO [production-cleanup])
-# camera.project_ray_origin(get_viewport().get_mouse_position())
+###########################################################################
