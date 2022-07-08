6f1cafa1f97f706da30a34285a9c0672470d4042
Aziroshin <aziroshin@gmx.net>
2022-07-08 14:55:21 +0200

    ✨ The n-gon now works properly, but the CSGNgon system also changed: It's now centered by default, and the vertices are calculated directly, without further translation (except for the offset). Also, the scale option is gone for now and has been replaced by a circumradius option.

--- dev/visualization/VisualizationNoodle/CSGNgon.gd
+++ dev/visualization/VisualizationNoodle/CSGNgon.gd
@@ -9,147 +9,55 @@
 extends CSGPolygon3D
 
 const DEFAULT_SIDES := 3
-const DEFAULT_SIDE_SCALE := 1.0
-const DEFAULT_OFFSET := Vector2(0, 1)
-const DEFAULT_CENTERED := true
+const DEFAULT_CIRCUMRADIUS := 1.0
+const DEFAULT_OFFSET := Vector2(0, 0)
 
 @export var sides := DEFAULT_SIDES:
 	set(value):
 		sides = value
 		_update()
-@export var side_scale := DEFAULT_SIDE_SCALE:
+@export var circumradius := DEFAULT_CIRCUMRADIUS:
 	set(value):
-		side_scale = value
+		circumradius = value
 		_update()
 @export var offset := DEFAULT_OFFSET:
 	set(value):
 		offset = value
 		_update()
-@export var centered := DEFAULT_CENTERED:
-	set(value):
-		centered = value
-		_update()
 
-# Returns a `PoolVector2array` of angle-adjusted basic vectors, with
-# each corresponding to a vertice in an n-gon.
-func ngon_vertices(n: int, side_scale: float) -> Array[Vector2]:
-	# Maybe this will help to get a good n-gon:
-	# https://math.stackexchange.com/questions/41940/is-there-an-equation-to-describe-regular-polygons
-	# https://www.geogebra.org/m/cXXGKUQk
-	
-	if n < 3:
-		return Array()
-		
-	# Vertex on apothem middle.
-	#var vertices: Array[Vector2] = Array([Vector2(side_scale - side_scale / 2, 0)])
-	
+# Returns an array with one vector per vertex in a 2D n-gon,
+# whereas each vector points from Vector2(0, 0) to the origin
+# of the vertex, thus describing the n-gon.
+func ngon_vertices(n: int, circumradius: float) -> Array[Vector2]:
 	var vertices: Array[Vector2] = Array()
+	if n < 3:
+		return vertices
+		
+	var rad_per_n = 2*PI/n
 	
-	for vertex_index in range(0, n+1):
-		
-		# It just produces a line, so, we've obviously done something wrong. xD
-		#var vector_length = deg2rad(360 / 2 * n)
-		
-		# Kinda works, but one side ends up being off in length, particularly
-		# with n < 32. <- Note: That error went away, and I'm not sure why. =\
-		#var vector_length = deg2rad(90+360/n*vertex_index)
-		var vector_length = deg2rad(360.0/n*vertex_index)
-		
-		# Note: This should give the side length, not the length of the
-		# vertice's origin vector, but it might still come in handy.
-		# 2 * sin(deg2rad(180/n))
-		
-		print("vector_length: %s, radians: %s" % [vector_length, deg2rad(vector_length)])
-		
-		var a := 2.0
+	for vertex_index in range(0, n):
 		vertices.append(Vector2(
-			sin(vector_length/2*PI) * side_scale/n,
-			-1 * cos(vector_length/2*PI) * side_scale/n
+			sin(rad_per_n * vertex_index) * circumradius,
+			cos(rad_per_n * vertex_index) * circumradius
 		))
-		
 	return vertices
-	
-# Takes a polygon for reference and a `Vector2` representing a vertex and
-# returns the resultant of the last vertex in the polygon and the specified
-# vertex. If the polygon has no vertices, the specified vertex is returned.
-func new_vertex(reference_polygon: Array[Vector2], new_vector: Vector2) -> Vector2:
-	var new_vector_transformed = new_vector
-	if reference_polygon.size() > 0:
-		new_vector_transformed = reference_polygon[-1] + new_vector
-	return new_vector_transformed
-
-# Takes a `PoolVector2Array` of vertices and returns a `PoolVector2Array`
-# with each vertice added to the preceding one.
-func create_resultant_polygon_from_vertices(vertices: Array[Vector2]) -> Array[Vector2]:
-	var new_polygon: Array[Vector2] = Array()
-	for vertex in vertices:
-		new_polygon.append(new_vertex(new_polygon, vertex))
-	return new_polygon
 
 func offset_polygon(vertices: Array[Vector2], offset: Vector2) -> Array[Vector2]:
 	var offset_polygon: Array[Vector2] = Array()
 	for vertex in vertices:
 		offset_polygon.append(vertex - offset)
 	return offset_polygon
-	
-func _get_greatest_y(vertices: Array[Vector2]) -> float:
-	assert(vertices.size() > 0, "The vertices array shouldn't be empty.")
-	
-	if vertices.size() == 0:
-		push_error(
-			"Attempted to get the greatest `y` component from an empty array of vertices. " +
-			"Returning `0.0`."
-		)
-		return 0.0
-	
-	# In case all vertices are negative, we definitely don't want to initialize
-	# with `0.0`, as that'd always be greater than any ys we'll find. Instead,
-	# we initialize with the first y we can get from the actual data.
-	var greatest_y_so_far := vertices[0].y
-	# This redundantly checks against the vertice we just initialized from.
-	# I'm unsure whether it's worth it to prevent it, unless there's a clever
-	# way to do it that doesn't involve `vertices.size() > 1` or whatever. xD
-	for vertice in vertices:
-		if vertice.y > greatest_y_so_far:
-			greatest_y_so_far = vertice.y
-	return greatest_y_so_far
-	
-#func center_polygon(vertices: Array[Vector2], offset: Vector2) -> Array[Vector2]:
-#	var _polygon: Array[Vector2]
-
-func create_positive_ngon(
-	n: int = DEFAULT_SIDES,
-	side_scale: float = DEFAULT_SIDE_SCALE,
-	offset: Vector2 = DEFAULT_OFFSET
-) -> Array[Vector2]:
-	var resultant_polygon: Array[Vector2] = create_resultant_polygon_from_vertices(ngon_vertices(n, side_scale))
-	if not offset == Vector2(0, 0):
-		return offset_polygon(resultant_polygon, offset)
-	return resultant_polygon
-
-func create_centered_ngon(
-	n: int = DEFAULT_SIDES,
-	side_scale: float = DEFAULT_SIDE_SCALE
-) -> Array[Vector2]:
-	# No offset, as we'll center it anyway.
-	var ngon: Array[Vector2] = create_positive_ngon(n, side_scale, Vector2(0, 0))
-	print("greatest_y: %s" % _get_greatest_y(ngon))
-	return offset_polygon(ngon, Vector2(0, _get_greatest_y(ngon) / 2))
 
 func create_ngon(
 	n: int = DEFAULT_SIDES,
-	side_scale: float = DEFAULT_SIDE_SCALE,
+	circumradius: float = DEFAULT_CIRCUMRADIUS,
 	offset: Vector2 = DEFAULT_OFFSET,
-	centered: bool = DEFAULT_CENTERED
 ) -> Array[Vector2]:
-	if centered:
-		return create_centered_ngon(n, side_scale)
-	return create_positive_ngon(n, side_scale, offset)
+	return offset_polygon(ngon_vertices(n, circumradius), offset)
 
 # Updates the n-gon.
 func _update():
-	polygon = create_ngon(sides, side_scale, offset)
-	print("ngon: %s" % polygon)
+	polygon = create_ngon(sides, circumradius, offset)
 
 func _enter_tree():
 	_update()
