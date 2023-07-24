0f2a927f2ef0bd113477c5fe429cdc57e5505ecb
Aziroshin <aziroshin@gmx.net>
2023-07-24 18:12:03 +0200

    🚧 Improved handling of non-existent scene paths interacting with the scene picker's scene path save system.

--- main_scenes/main_scenes_lib/scene_picker.gd
+++ main_scenes/main_scenes_lib/scene_picker.gd
@@ -13,8 +13,53 @@ const PICKED_SCENE_PATH_CONFIG_KEY := ScenePickerConfigModel.PICKED_SCENE_PATH_C
 # This button will be instantiated for every scene in `MAIN_SCENES_PATH`.
 const SetSceneButton: Resource = preload(
 	"%smain_scenes_lib/set_scene_button.tscn" % MAIN_SCENES_PATH)
-
-
+	
+	
+# TODO [bug]: ResourceLoader.exists returns true, even if the `resource_type`
+#  is wrong. Because of that this function currently returns true even if the
+#  type is wrong.
+func resource_path_is_sane_with_warning(
+	path: String,
+	called_from_getter: bool, # If false, it's a setter.
+	descriptive_term := "",
+	resource_type: String = "",
+) -> bool:
+		var path_exists := ResourceLoader\
+			.exists(path) == true
+		var is_proper_type := ResourceLoader\
+			.exists(path, resource_type) == true
+		
+		# Assertion for debugging the function.
+		#assert(false, "path_exists: %s, is_proper_type: %s, type: %s, called_from_getter: %s, path: %s"\
+		#	% [path_exists, is_proper_type, resource_type, str(called_from_getter), path])
+		
+		if called_from_getter:
+			if not path_exists:
+				push_warning("Default %s returned, " % descriptive_term
+					+ "because stored scene path doesn't exist: '%s'." % path
+				)
+				return false
+			if path_exists and not is_proper_type:
+				push_warning("Default %s " % descriptive_term
+					+ "returned, because stored %s exists " % descriptive_term
+					+ "but isn't of type '%s': '%s'." % [resource_type, path]
+				)
+				return false
+		else: # Called from setter.
+			if not path_exists:
+				push_warning("Storing non-existent %s " % descriptive_term
+					+ ": '%s'." % path
+				)
+				return false
+			if path_exists and not is_proper_type:
+				push_warning("Storing % which isn't of the " % descriptive_term
+					+ "specified type (%s): %s" % [resource_type, path]
+				)
+				return false
+		
+		return true
+		
+		
 # The scene we want to attach picked scenes to.
 @onready var root_scene: Node3D =\
 	# Note: This default value is set with a `DevMenu` overlay parent node in
@@ -41,12 +86,31 @@ const SetSceneButton: Resource = preload(
 
 @onready var picked_scene_path: String:
 	get:
-		return config.get_value(
+		var stored_scene_path: String = config.get_value(
 			PICKED_SCENE_PATH_CONFIG_KEY,
-			"%sDefault.tscn" % MAIN_SCENES_PATH)
-	set(value):
-		config.set_value(PICKED_SCENE_PATH_CONFIG_KEY, value)
-
+			DEFAULT_SCENE_PATH
+		)
+		
+		if resource_path_is_sane_with_warning(
+			stored_scene_path, true, "scene path", "PackedScene"
+		):
+			return stored_scene_path
+		
+		return DEFAULT_SCENE_PATH
+	
+	set(path):
+		resource_path_is_sane_with_warning(
+			path, false, "scene path", "PackedScene"
+		)
+		
+		# If we'd store the default scene path in cases where the getter
+		# returned the default because a non-existent path was found in storage,
+		# we'd be overwriting that path, even though it could prove useful for
+		# debugging.
+		if not path == DEFAULT_SCENE_PATH:
+			config.set_value(PICKED_SCENE_PATH_CONFIG_KEY, path)
+		
+		
 # Edit this if you have scenes in your `MAIN_SCENES_PATH` you don't want to
 # have buttons on the picker.
 @onready var scene_blacklist: Array[String] = [
@@ -74,7 +138,7 @@ func _ready():
 		if file_name.get_extension() == "tscn" and not file_res_path in scene_blacklist:
 			print(file_res_path)
 			print(str(scene_blacklist))
-			var new_button := SetSceneButton.instantiate()
+			var new_button: HBoxContainer = SetSceneButton.instantiate()
 			new_button.init(file_res_path, file_name.get_basename())
 			new_button.scene_picked.connect(_on_set_scene_button_button_up)
 			add_child(new_button)
