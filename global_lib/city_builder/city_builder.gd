5e5a509e3973494dcfc314976b9546981a38dc8c
Aziroshin <aziroshin@gmx.net>
2023-07-24 19:02:15 +0200

    🚧 Start of `city_builder`, which contains some early dabblings at fleshing out a data model, and later perhaps also functionality, pertaining to tying together city building with various sub-systems such as building generators. Currently, this is focused on modeling metadata to communicate to such generators, e.g. regarding the shape of a building, or what kinds of facades it has.

---
+++ global_lib/city_builder/city_builder.gd
@@ -0,0 +1,125 @@
+extends RefCounted
+class_name CityBuilder
+
+
+enum HouseSideKinds {
+	FREE,
+	ADJACENT,
+	BERLIN_CORNER,
+	# MAIN #  Maybe useful for special houses.
+}
+
+
+enum HouseSideExposedness {
+	BUSY,
+	PRIVATE
+}
+
+
+# Archived.
+# Code-doodle for builder-pattern house features. Also, the idea was to have
+# an object that can be passed on to generators without having to mess too much
+# with the limitations of the type hint system in the absence of generics. One
+# big object with all the features would do that, but it'd also make things
+# messy if other projects need to add their own features for their generators.
+# Furthermore, it doesn't preserve the respective exclusiveness of "kind" and
+# "exposedness" items, and adding the enums would greatly reduce the readability
+# of the builder pattern approach.
+class HouseSideFeatures:
+	class Kind:
+		var delegator: HouseSideFeatures
+		var free = false
+		var adjacent = false
+		var berlin_corner = false
+		
+		func _init(delegator: HouseSideFeatures):
+			self.delegator = delegator
+		
+		func set_free(value: bool) -> HouseSideFeatures:
+			free = value
+			return delegator
+			
+		func set_adjacent(value: bool) -> HouseSideFeatures:
+			adjacent = value
+			return delegator
+		
+		
+	class Exposedness:
+		var delegator: HouseSideFeatures
+		var busy = false
+		var private = false
+		
+		func _init(delegator: HouseSideFeatures):
+			self.delegator = delegator
+		
+		func set_busy(value: bool) -> HouseSideFeatures:
+			busy = value
+			return delegator
+			
+	var kind: Kind
+	var exposedness: Exposedness
+	
+	func _init():
+		kind = Kind.new(self)
+		exposedness = Exposedness.new(self)
+		
+		
+# Note: The points of a house side describe a 3D line, whereas `y` will need
+# special consideration in generators regarding ground-level and sous-de-terre
+# facade bits.
+class HouseSide:
+	var points: PackedVector3Array
+	var start: Vector3:
+		get:
+			return points[0]
+	var end: Vector3:
+		get:
+			return points[len(points)-1]
+	var start_to_end: Vector3:
+		get:
+			return end - start
+	
+	func _init(
+		points: PackedVector3Array,
+		kind: HouseSideKinds,
+		exposedness: HouseSideExposedness
+	):
+		assert(len(points) >= 2)
+	
+	
+# TODO: Holds all `HouseSide` objects for a house and includes sanity
+#  checks to make sure the sides fit in a way it makes sense for a house.
+#  Might also include arrays for various outlines across all the sides, e.g.
+#  the outline of the foundation or the top outline of all the sides put
+#  together, or roof outlines, etc.
+#  Might also include convenience methods or getters, e.g. whether the number
+#  of sides is even or somesuch.
+class HouseFrame:
+	var sides: Array[HouseSide]
+	
+	func _init(sides: Array[HouseSide]):
+		pass
+		# Assert/test/push_error whether the set of points from all the
+		# house sides form a loop (good) or not (bad).
+	
+	
+# Idea-stub & basic "interface" for lowest-common-denominator type erasure
+# for "Building" classes. Time will tell what functionality it'll have, if
+# any.
+class Building:
+	pass
+	
+	
+class SimpleRowHouse extends Building:
+	var frame: HouseFrame
+	
+	func _init(
+		frame: HouseFrame
+	):
+		pass
+		
+		
+func generate_simple_row_house(
+	
+) -> Building:
+	return Building.new()
