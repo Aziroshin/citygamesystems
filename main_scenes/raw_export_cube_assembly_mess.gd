6bb2238ef20ed24bacb6507bc277864ec76a0af9
Aziroshin <aziroshin@gmx.net>
2023-08-14 16:09:41 +0200

    ♻️ Keeping the mess tidy. :p

--- main_scenes/raw_export_cube_assembly_mess.gd
+++ main_scenes/raw_export_cube_assembly_mess.gd
@@ -17,32 +17,50 @@ func load_and_get_test_cube() -> Resource:
 	return load_and_get_raw_export(test_cube_path)
 
 
+func add_indices_to_surface_arrays(surface_arrays: Array, indices: PackedInt64Array):
+	surface_arrays[ArrayMesh.ARRAY_INDEX] = indices
+
+
+func add_vertices_to_surface_arrays(surface_arrays: Array, vertices: PackedVector3Array):
+	var multi_quad := AMultiQuad.new()
+	for i_face in range(len(vertices) / 4):
+		var offset = i_face * 4
+		var quad := AQuad.new(
+			vertices[offset],
+			vertices[offset+1],
+			vertices[offset+2],
+			vertices[offset+3]
+		)
+		multi_quad.add_quad(quad)
+	multi_quad.apply_all()
+
+	var surface_array_vertex := multi_quad.get_array_vertex()
+	surface_arrays[ArrayMesh.ARRAY_VERTEX] = surface_array_vertex
+
+
+func add_normals_to_surface_arrays(surface_arrays: Array, normals: PackedVector3Array):
+	surface_arrays[ArrayMesh.ARRAY_NORMAL] = normals
+
+
+func add_uvs_to_surface_arrays(surface_arrays: Array, uvs: PackedVector2Array):
+	surface_arrays[ArrayMesh.ARRAY_TEX_UV] = uvs
+
+
 func _mess(show_debug_overlay) -> Node3D:
 	var file := FileAccess.get_file_as_string(test_cube_path)
 	var obj: Dictionary = JSON.parse_string(file)
 	var object_data := RawExport.RawObjectData_from_json(file)
 	
-	var multi_quad := AMultiQuad.new()
-	for i_face in range(len(object_data.vertices) / 4):
-		var offset = i_face * 4
-		var quad := AQuad.new(
-			object_data.vertices[offset],
-			object_data.vertices[offset+1],
-			object_data.vertices[offset+2],
-			object_data.vertices[offset+3]
-		)
-		multi_quad.add_quad(quad)
-	multi_quad.apply_all()
-
 	var surface_arrays := []
 	surface_arrays.resize(ArrayMesh.ARRAY_MAX)
-	var surface_array_vertex := multi_quad.get_array_vertex()
-	surface_arrays[ArrayMesh.ARRAY_VERTEX] = surface_array_vertex
+	add_vertices_to_surface_arrays(surface_arrays, object_data.vertices)
+	add_indices_to_surface_arrays(surface_arrays, object_data.indices)
+	add_normals_to_surface_arrays(surface_arrays, object_data.normals)
 	
 	var array_mesh_node = CityGeoFuncs.get_array_mesh_node(surface_arrays)
 	if show_debug_overlay:
 		array_mesh_node.add_child(ADebugOverlay.new().visualize_array_vertex(
-			multi_quad.get_array_vertex()
+			surface_arrays[ArrayMesh.ARRAY_VERTEX]
 		))
 	return array_mesh_node
 
