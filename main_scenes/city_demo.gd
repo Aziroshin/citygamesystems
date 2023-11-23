86f6c5dacaf04b305cc3c5b61c6780faed5d6a07
Aziroshin <aziroshin@gmx.net>
2023-11-23 13:30:09 +0100

    🚧 City demo skeleton. Spawns `Cavedig` needles on click and has two street tool instances and corresponding buttons to rudimentarily verify that the arbiter works.

--- main_scenes/city_demo.gd
+++ main_scenes/city_demo.gd
@@ -1,6 +1,19 @@
 extends Node3D
 
+@onready var map: PlaneMap = $Map
 
 
+func _ready() -> void:
 	pass
 	
+func _process(delta) -> void:
+	pass
+
+func _on_map_mouse_button(
+	camera: Camera3D,
+	event: InputEvent,
+	mouse_position: Vector3,
+	normal: Vector3,
+	shape: int
+) -> void:
+	Cavedig.needle(self.map, Transform3D(Basis(), mouse_position))
