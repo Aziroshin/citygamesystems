501e8a718ccb253171dfe21c91f1bdae6b9ccae2
Aziroshin <aziroshin@gmx.net>
2024-05-19 04:05:23 +0200

    👀 Got rid of some broken semi-pseudo code.

--- main_scenes/city_demo_lib/world_objects/street/street_segment.gd
+++ main_scenes/city_demo_lib/world_objects/street/street_segment.gd
@@ -2,7 +2,3 @@ extends WorldObject
 class_name StreetSegmentWorldObject
 
 var street_segment: CityBuilder.StreetSegment
-
-var world_object := WorldObject.get_from_collider_or_null(collider)
-if world_object:
-	pass
